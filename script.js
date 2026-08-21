af7defe8358d3c90dac5f495e9ac4765ade3c504
sukipantai <vubergen1@gmail.com>
2026-08-21 12:52:14 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const songs = [
-  ["Golden Hour", "JVKE", 209, "Golden Hour.mp3"],
+  ["Golden Hour", "JVKE", 209, "goldenhour.mp3"],
   ["Ceilings", "Lizzy McAlpine", 194, "Ceilings.mp3"],
   ["Death Bed", "Powfu ft. beabadoobee", 173, "Death Bed.mp3"],
 ];
